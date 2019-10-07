b684d1b36c6857494cd1af3e2d75e07bd88d6b9e
Rosalis Freebairn <rfreeb@artic.edu>
2019-10-07 18:03:36 -0500

    Changed wording in the first query prompt. Added Spotify constructer with the client and secret client IDs.

--- liri.js
+++ liri.js
@@ -2,36 +2,51 @@ const dotenv = require("dotenv").config();
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const spotify = require('node-spotify-api');
+const Spotify = require("node-spotify-api");
 
 // Set axios GET method
+
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
 
 // Make userInput function for inquirer
 
 function userInput() {
-  inquirer.prompt([
-    {
-      name: "queryType",
-      type: "list",
-      message: "Do you want to search songs or upcoming concerts?",
-      choices: ["Song", "Upcoming Concerts"]
-    },
-    {
-      name: "queryString",
-      message: "Enter song or artist/band for upcoming concerts"
-    }
-  ])
-  .then(answers => {
-    let queryUrl = ""; //This will be passed to the api call as the search term.
-    if (answers.queryType === "Songs") {
-      queryUrl = "";
-    } else if (answers.queryType === "Artist/Band") {
-      queryUrl = "https://api.seatgeek.com/2/events?client_id=process.env.SEATGEEK_ID";
-    } else {
-      console.log("Whoops, can't find that...");
+  inquirer
+    .prompt([
+      {
+        name: "queryType",
+        type: "list",
+        message: "Do you want to search songs or artist/band?",
+        choices: ["Songs", "Artist/Band"]
+      },
+      {
+        name: "queryString",
+        message: "Enter song or artist/band for upcoming concerts"
+      }
+    ])
+    .then(answers => {
+      console.log(answers);
+      if (answers.queryType === "Songs") {
+        spotify.search({ type: "track", query: "Don't Turn Around" }, function(
+          err,
+          data
+        ) {
+          if (err) {
+            return console.log("Error occurred: " + err);
+          }
+
+          console.log(data);
+        });
+      } else if (answers.queryType === "Artist/Band") {
+        queryUrl =
+          "https://api.seatgeek.com/2/events?client_id=process.env.SEATGEEK_ID";
+      } else {
+        console.log("Whoops, can't find that...");
+      }
     });
-    
-  }
 }
 
 // Set Spotify API and SeetGeek API as queries?
@@ -46,7 +61,7 @@ function userInput() {
 
 // Set the default song to "Don't Turn Around by Ace of Base" if user does not provide a song in the Music Artist query.
 
-// Set the default concert to "Don't Turn Around by Ace of Base" if user does not provide a song in the Music Artist query.
+// Set the default concert to "Lady Gaga" if user does not provide an artist or band in the Music Artist query.
 
 // Set query parameters in the userInput function
 
@@ -54,7 +69,7 @@ function userInput() {
 
 // Make a fs function so every query is appended into a txt file
 
-// Call functions 
+// Call functions
 
 // Explain entire process in README
 userInput();
